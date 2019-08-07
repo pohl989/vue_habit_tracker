9c6f0121153cef513c6a2609cc866495e2a8543d
pohl989 <pohl989@gmail.com>
2019-08-06 21:15:26 -0600

    add vuetify

---
+++ src/components/HabitList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p class="username">{{username}} Taco</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.username {
+  font-size: 50pt;
+  color: red;
+}
+</style>
